f14f59d45e89d46558efbd7fb436b83f95caefc1
Nikita Kinash <nka2018nn@gmail.com>
2022-02-24 23:51:38 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,67 @@
+# EASY S3 PACKAGE
+
+---------------------------------------
+
+This package makes using s3 easier.
+
+## Quick Examples
+
+### Simple S3 usage
+
+```php
+<?php
+// Require the Composer autoloader.
+require 'vendor/autoload.php';
+
+use nka20\easyS3\Service;
+
+// Instantiate an S3 service.
+$s3 = new Service(
+    'key',
+    'secret',
+    'ru-central1'
+);
+```
+
+### Upload file
+
+```php
+$key = '/storage/path/text.txt';
+
+$s3->putObject('bucket', $key, 'Hello, Simple S3');
+```
+
+### Download file
+
+```php
+$s3->getObject('bucket', $key);
+```
+
+### Delete object
+
+```php
+$s3->deleteObject('bucket', $key);
+```
+
+## _It's easy, isn't it?_
+
+___
+
+**Docs**:
+ * _listBuckets()_
+ * _listObjects($bucket)_
+ * _putObject($bucket, $key, $data)_
+ * _getObject($bucket, $key): string_
+ * _deleteObject($bucket, $key)_
+ * _deleteObjects($bucket, $keys)_
+ * _createBucket($bucket, $args)_
+ * _deleteBucket($bucket, $args)_
+ 
+_For advanced usage uou can get native S3Client_
+```php
+$awsS3Client = $s3->getClient();
+$awsS3Client->...();
+```
+
+---
+For details checkout https://github.com/aws/aws-sdk-php.
---
+++ src/Service.php
@@ -0,0 +1,171 @@
+<?php
+
+declare(strict_types=1);
+
+namespace nka20\EasyS3;
+
+
+use Aws\Result;
+use Aws\S3\S3Client;
+
+class Service
+{
+    protected S3Client $client;
+
+    /**
+     * For easy s3 use you should
+     * only give your credits to constructor
+     * and region
+     * @param string $key
+     * @param string $secret
+     * @param string $region
+     * @param string $version
+     * @param string $endpoint
+     * @param array $config
+     */
+    public function __construct(
+        string $key,
+        string $secret,
+        string $region,
+        string $version = 'latest',
+        string $endpoint = '',
+        array $config = []
+    )
+    {
+        $config['credentials'] = [
+            'key' => $key,
+            'secret' => $secret,
+        ];
+
+        $config['region'] = $region;
+        $config['version'] = $version;
+
+        if (!empty($endpoint)) {
+            $config['endpoint'] = $endpoint;
+            $config['http']['verify'] = false;
+        }
+
+        $this->client = new S3Client($config);
+    }
+
+    /**
+     * @return Result
+     */
+    public function listBuckets(): Result
+    {
+        return $this->client->listBuckets();
+    }
+
+    /**
+     * @param string $bucket
+     * @return Result
+     */
+    public function listObjects(string $bucket): Result
+    {
+        return $this->client->listObjects([
+            'Bucket' => $bucket
+        ]);
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $key
+     * @param string $data
+     * @return Result
+     */
+    public function putObject(string $bucket, string $key, string $data): Result
+    {
+        return $this->client->putObject([
+            'Bucket' => $bucket,
+            'Key' => $key,
+            'Body' => $data,
+        ]);
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $key
+     * @return string
+     */
+    public function getObject(string $bucket, string $key): string
+    {
+        $result = $this->client->getObject([
+            'Bucket' => $bucket,
+            'Key' => $key,
+
+        ]);
+
+        return $result['Body']->getContents();
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $key
+     * @return Result
+     */
+    public function deleteObject(string $bucket, string $key): Result
+    {
+        return $this->client->deleteObjects([
+            'Bucket'  => $bucket,
+            'Delete' => [
+                'Objects' => [
+                    [
+                        'Key' => $key
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+
+    /**
+     * @param string $bucket
+     * @param array $keys
+     * @return Result
+     */
+    public function deleteObjects(string $bucket, array $keys): Result
+    {
+        return $this->client->deleteObjects([
+            'Bucket'  => $bucket,
+            'Delete' => [
+                'Objects' => [
+                    [
+                        'Key' => $keys
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @param string $name
+     * @param array $args
+     * @return Result
+     */
+    public function createBucket(string $name, array $args = []): Result
+    {
+        $args['bucket'] = $name;
+        return $this->client->createBucket($args);
+    }
+
+    /**
+     * @param string $name
+     * @param array $args
+     * @return Result
+     */
+    public function deleteBucket(string $name, array $args = []): Result
+    {
+        $args['bucket'] = $name;
+        return $this->client->createBucket($args);
+    }
+
+    /**
+     * For advanced S3 Client use
+     * @return S3Client
+     */
+    public function getClient(): S3Client
+    {
+        return $this->client;
+    }
+
+}
